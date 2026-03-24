f45805c366639d18263018890c3cc7ca492b7784
louClimber <lourymello@gmail.com>
2026-03-24 10:35:43 +0100

    Initial commit

---
+++ data.js
@@ -0,0 +1,640 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-01",
+      "name": "01",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": -2.005752666582069,
+        "pitch": -0.4104232785163653,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.8260631707758073,
+          "pitch": 0.11895229612987102,
+          "rotation": 0,
+          "target": "2-02",
+        }
+      ],
+      "dronHotspots": [
+        {
+          "yaw": -1.1995216622393716,
+          "pitch": -0.6998552470930424,
+          "rotation": 0,
+          "target": "1-011"
+        }
+      ],
+      "webHotspots": [
+        {
+          "yaw": -2.2247530310444894,
+          "pitch": -0.2691438872409666,
+          "url": "https://www.aytoaliaga.com/5-3-la-formacion-mosqueruela/",
+          "title": "5.3. LA FORMACION MOSQUERUELA"
+        },
+        {
+          "yaw": 1.516753273665791,
+          "pitch": -0.021920501397652004,
+          "url": "https://www.aytoaliaga.com/5-2-la-transgresion-marina-del-inicio-del-cretacico-superior/",
+          "title": "5.2. LA TRANSGRESIÓN MARINA DEL INICIO DEL CRETÁCICO SUPERIOR"
+        }
+      ],
+      "homeHotspots": [
+        {
+          "yaw": -2.761066510487515,
+          "pitch": -0.8033857776387645,
+          "url": "https://www.aytoaliaga.com/", /*cambiar*/
+          "title": "Portada",
+          
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-011",
+      "name": "6.1.LOS PLIEGUES SERPENTEANTES DISARMÓNICOS",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": -2.0627846468039195,
+        "pitch": 0.5401236191042855,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.0290715824443417,
+          "pitch": 0.534268519854141,
+          "rotation": 3.141592653589793,
+          "target": "0-01",
+          "title": "Retornar"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.9109930262361274,
+          "pitch": 0.16484675847177144,
+          "title": "6.1",
+          "text": "Text"
+        }
+      ],
+      "dronHotspots": [],
+      "webHotspots": [
+        {
+          "yaw": -1.9109930262361274,
+          "pitch": 0.16484675847177144,
+          "url": "https://www.aytoaliaga.com/6-1-los-pliegues-serpenteantes-disarmonicos/",
+          "title": "6.1. LOS PLIEGUES SERPENTEANTES DISARMÓNICOS"
+        }
+      ]
+    },
+    {
+      "id": "2-02",
+      "name": "02",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": -2.587227685715174,
+        "pitch": -0.3736271917473353,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.2059728027116368,
+          "pitch": 0.14832187097808713,
+          "rotation": 0,
+          "target": "4-03"
+        },        
+      ],
+      "infoHotspots": [],
+      "dronHotspots": [
+        {
+          "yaw": -1.8779047758895366,
+          "pitch": -0.786080401464325,
+          "rotation": 0,
+          "target": "3-021"
+        }
+      ],
+      "webHotspots": [
+        {
+          "yaw": -2.4042200992708533,
+          "pitch": -0.4899594547340076,
+          "url": "https://www.aytoaliaga.com/5-4-la-erosion-diferencial-en-las-calizas-de-la-fm-mosqueruela/",
+          "title": "5.4. LA EROSIÓN DIFERENCIAL EN LAS CALIZAS DE LA FM. MOSQUERUELA",          
+        }
+      ]
+    },
+    {
+      "id": "3-021",
+      "name": "6.4. PEÑA DEL BARBO",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": -1.4976216551875616,
+        "pitch": 0.20309960199656985,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.5530831405781846,
+          "pitch": 0.3349088730011083,
+          "rotation": 3.141592653589793,
+          "target": "2-02",
+          "title": "Retornar"
+        }
+      ],
+      "infoHotspots": [],
+      "dronHotspots": [],
+      "webHotspots": [
+        {
+          "yaw": -1.2977659478666563,
+          "pitch": -0.30804399019733,
+          "url": "https://www.aytoaliaga.com/6-4-la-pena-del-barbo/",
+          "title": "6.4. PEÑA DEL BARBO"          
+        }
+      ]
+    },
+    {
+      "id": "4-03",
+      "name": "03",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": -2.8119553291871746,
+        "pitch": -0.24355516738835803,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.6616732166621073,
+          "pitch": 0.14594839911319113,
+          "rotation": 0,
+          "target": "7-04"
+        }        
+      ],
+      "infoHotspots": [],
+      "dronHotspots": [
+        {
+          "yaw": -2.9868001442852314,
+          "pitch": -0.6978806658350312,
+          "rotation": 0,
+          "target": "5-031"
+        }
+      ],
+      "webHotspots": []
+    },
+    {
+      "id": "5-031",
+      "name": "6.3. PLIEGUE SERPENTEANTE EN CRESTA",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": -2.8771201413104468,
+        "pitch": -0.2987811887962742,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [],
+      "infoHotspots": [],
+      "dronHotspots": [
+        {
+          "yaw": -3.06896736706479,
+          "pitch": -0.676652275655659,
+          "rotation": 0,
+          "target": "6-032"
+        }
+      ],
+      "webHotspots": [
+        {
+          "yaw": 2.9922492732661183,
+          "pitch": 0.18556776706271805,
+          "url": "https://www.aytoaliaga.com/6-3-pliegue-serpenteante-en-cresta/",
+          "title": "6.3. PLIEGUE SERPENTEANTE EN CRESTA",          
+        }
+      ]
+    },
+    {
+      "id": "6-032",
+      "name": "6.3. PLIEGUE SERPENTEANTE EN CRESTA",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 1.5032060086599994,
+        "pitch": 1.1459013218899976,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.31942465776954876,
+          "pitch": 0.9816100450828742,
+          "rotation": 3.141592653589793,
+          "target": "4-03",
+          "title": "Retornar"
+        }
+      ],
+      "infoHotspots": [],
+      "dronHotspots": [],
+      "webHotspots": [
+        {
+          "yaw": 2.312379922252254,
+          "pitch": 0.7838074783147917,
+          "url": "https://www.aytoaliaga.com/6-3-pliegue-serpenteante-en-cresta/",
+          "title": "6.3. PLIEGUE SERPENTEANTE EN CRESTA"          
+        }
+      ]
+    },
+    {
+      "id": "7-04",
+      "name": "04",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": -2.46452616713813,
+        "pitch": 0.006961850926466,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.9330499988184275,
+          "pitch": 0.1291856641961413,
+          "rotation": 0,
+          "target": "8-05"
+        }
+      ],
+      "infoHotspots": [],
+      "dronHotspots": [],
+      "webHotspots": [
+        {
+          "yaw": -1.46066111893334,
+          "pitch": 0.20043661824667538,
+          "title": "6.5. UN PLIEGUE SERPENTEANTE, DE BOLSILLO",
+          "url": "https://www.aytoaliaga.com/6-5-un-pliegue-serpenteante-de-bolsillo/"
+        }
+      ]
+    },
+    {
+      "id": "8-05",
+      "name": "05",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 2.825878172404342,
+        "pitch": -0.32265855508711994,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.778180292388731,
+          "pitch": 0.1572432846077767,
+          "rotation": 0,
+          "target": "10-06"
+        }        
+      ],
+      "infoHotspots": [],
+      "dronHotspots": [
+        {
+          "yaw": 2.484812627395046,
+          "pitch": -0.8342283428668225,
+          "rotation": 0,
+          "target": "9-051"
+        }
+      ],
+      "webHotspots": [
+        {
+          "yaw": 2.021917529486779,
+          "pitch": -0.28317518909807404,
+          "url": "https://www.aytoaliaga.com/6-6-el-pliegue-serpenteantede-mayor-escala/",
+          "title": "6.6. EL PLIEGUE SERPENTEANTE DE MAYOR ESCALA",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "9-051",
+      "name": "05.1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 0.7052554936630084,
+        "pitch": 0.2162653818946101,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.17238389853723923,
+          "pitch": 0.6333722051335133,
+          "rotation": 3.141592653589793,
+          "target": "8-05",
+          "title": "Retornar"
+        }
+      ],
+      "infoHotspots": [],
+      "dronHotspots": [],
+      "webHotspots": [
+        {
+          "yaw": 0.6899759236318026,
+          "pitch": -0.1837913638348283,
+          "url": "https://www.aytoaliaga.com/6-6-el-pliegue-serpenteantede-mayor-escala/",
+          "title": "6.6. EL PLIEGUE SERPENTEANTE DE MAYOR ESCALA",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "10-06",
+      "name": "06",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 2.2119647949548424,
+        "pitch": -0.3369033162114596,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.8192988053271355,
+          "pitch": 0.1755375264856962,
+          "rotation": 0,
+          "target": "12-07"
+        },        
+      ],
+      "infoHotspots": [],
+      "dronHotspots": [
+        {
+          "yaw": 1.8260004444133732,
+          "pitch": -0.823597105662386,
+          "rotation": 0,
+          "target": "11-061"
+        }
+      ],
+      "webHotspots": [
+        {
+          "yaw": 1.440256796823105,
+          "pitch": 0.14698546031035065,
+          "url": "https://www.aytoaliaga.com/5-5-la-formacion-barranco-de-los-degollados/",
+          "title": "5.5. LA FORMACIÓN BARRANCO DE LOS DEGOLLADOS",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "11-061",
+      "name": "06.1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 0.2312953803853972,
+        "pitch": 1.394390997118375,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.1388471382150787,
+          "pitch": 0.7476370573734687,
+          "rotation": 3.141592653589793,
+          "target": "10-06",
+          "title": "Retornar"
+        }
+      ],
+      "infoHotspots": [],
+      "dronHotspots": [],
+      "webHotspots": [
+        {
+          "yaw": 0.7797325795992425,
+          "pitch": 1.36145114073706,
+          "url": "https://www.aytoaliaga.com/6-6-el-pliegue-serpenteantede-mayor-escala/",
+          "title": "6.6. EL PLIEGUE SERPENTEANTE DE MAYOR ESCALA",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "12-07",
+      "name": "07",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 0.5360674985276237,
+        "pitch": -0.1908195755891775,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.4369797210205984,
+          "pitch": 0.125732628222729,
+          "rotation": 4.71238898038469,
+          "target": "0-01",
+          "title": "INÍCIO"
+        }
+      ],
+      "infoHotspots": [],
+      "homeHotspots":[
+        {
+          "yaw": 1.5614764015664289,
+          "pitch": -0.44443129686263205,
+          "url": "",
+          "title": "Portada",
+          "text": "Text"
+        }
+      ],
+      "dronHotspots": [],
+      "webHotspots": [
+        {
+          "yaw": 0.2992444386322255,
+          "pitch": -0.5157381845001368,
+          "url": "https://www.aytoaliaga.com/5-5-la-formacion-barranco-de-los-degollados/",
+          "title": "5.5. LA FORMACIÓN BARRANCO DE LOS DEGOLLADOS",
+          "text": "Text"
+        },        
+      ]
+    }
+  ],
+  "name": "El Estrecho de Aldehuela - Panoramas 360º - Virtual Tour",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
